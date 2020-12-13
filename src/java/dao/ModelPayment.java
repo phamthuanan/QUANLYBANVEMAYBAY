79491c4894bdec178ed95c0b01c642f3a442fb95
LinhMy000 <51374454+LinhMy000@users.noreply.github.com>
2020-12-13 21:41:30 +0700

    payment update

--- src/java/dao/ModelPayment.java
+++ src/java/dao/ModelPayment.java
@@ -5,8 +5,6 @@ import java.io.FileWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +14,97 @@ import model.Ticket;
 public class ModelPayment {
 
     private static Connection con = ConnectionDB.getConnection();
+
+    // Chức năng Thanh toán
+    public static int Payment(Receipt user) {
+        int mahd = 0;
+        String sql = "insert into hoadon values(null,?,?,?,?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, user.getMakh());
+            ps.setInt(2, user.getSlVe());
+            ps.setDouble(3, user.getDonGia());
+            ps.setString(4, user.getHttt());
+            ps.setDate(5, user.getNgayhd());
+            ps.execute();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                mahd = rs.getInt(1);	//Gia tri cua mahd moi duoc tu dong tao
+            }
+            ps.close();
+
+            //Them Ve
+            if (mahd != 0) {
+                for (int i = 0; i < user.getVe().size(); ++i) {
+                    Ticket ct = new Ticket();
+                    ct.setMahd(rs.getInt("mahd"));
+                    ct.setMave(rs.getInt("mave"));
+                    ct.setMacb(rs.getInt("macb"));
+                    ct.setMaghe(rs.getInt("maghe"));
+                    ct.setGiave(rs.getDouble("giave"));
+
+                    insertTicket(ct);
+                }
+            } else {
+                System.out.println("Lỗi thêm hóa đơn!");
+            }
+        } catch (Exception e) {
+            System.out.println("Lỗi thêm hóa đơn và vé!");
+            e.printStackTrace();
+        }
+        return mahd;
+    }
+
+    // Xuất hóa đơn
+    public static void xuatHD(int mahd) {
+        try {
+            // Đường dẫn đến nơi muốn xuất file + tên file.txt
+            FileWriter fw = new FileWriter("C:\\Users\\LENOVO\\Desktop\\TestXuatHD.txt");
+
+            Receipt h = getHD(mahd);
+            fw.write("Welcome to My Website! \r\n");
+            SimpleDateFormat fm = new SimpleDateFormat("dd-MM-yyyy");
+            fw.write("Ngày: " + fm.format(h.getNgayhd()) + "\r\n");
+            fw.write("Khách Hàng: " + CustomerDao.getTenKH(h.getMakh()) + "\r\n");
+            fw.write("Mã Vé \tMã CB \tMã Ghế \tGiá Vé \r\n");
+            fw.write("Tên Sách \t\tSố Lượng \tThành Tiền \r\n");
+            for (int i = 0; i < h.getVe().size(); i++) {
+                fw.write(h.getVe().get(i).getMave() + "\t");
+                fw.write(h.getVe().get(i).getMacb() + "\t");
+                fw.write(h.getVe().get(i).getMaghe() + "\t");
+                fw.write(h.getVe().get(i).getGiave() + "VNĐ \t");
+            }
+            fw.write("\r\nTổng cộng: \t" + h.getDonGia() + "VNĐ \t");
+            fw.close();
+        } catch (Exception e) {
+            System.out.println("Lỗi xuất hd" + e);
+        }
+        System.out.println("Success...");
+    }
+
+    public static int insertTicket(Ticket c) {
+        int row = 0;
+        String sql = "insert into ve values(?,?,?,?,?)";
+        try {
+            PreparedStatement psi = con.prepareStatement(sql);
+            psi.setInt(1, c.getMave());
+            psi.setInt(2, c.getMahd());
+            psi.setInt(3, c.getMacb());
+            psi.setInt(4, c.getMaghe());
+            psi.setDouble(5, c.getGiave());
+            row = psi.executeUpdate();
+            psi.close();
+            /*
+            if (row != 0) {
+                System.out.println("Them Ticket thanh cong !");
+            }
+             */
+        } catch (Exception e) {
+            System.out.println("Lỗi thêm vé!");
+            e.printStackTrace();
+        }
+        return row;
+    }
 
     public static Receipt getHD(int ma) {
         Receipt user = new Receipt();
@@ -90,92 +179,4 @@ public class ModelPayment {
         return list;
     }
 
-    public static int insertReceipt(Receipt user) {
-        int mahd = 0;
-        String sql = "insert into hoadon values(null,?,?,?,?,?)";
-        try {
-            PreparedStatement ps = con.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
-            ps.setInt(1, user.getMakh());
-            ps.setInt(2, user.getSlVe());
-            ps.setDouble(3, user.getDonGia());
-            ps.setString(4, user.getHttt());
-            ps.setDate(5, user.getNgayhd());
-            ps.execute();
-            ResultSet rs = ps.getGeneratedKeys();
-            if (rs.next()) {
-                mahd = rs.getInt(1);	//Gia tri cua mahd moi duoc tu dong tao
-            }
-            ps.close();
-
-            //Them Ve
-            if (mahd != 0) {
-                for (int i = 0; i < user.getVe().size(); ++i) {
-                    Ticket ct = new Ticket();
-                    ct.setMahd(rs.getInt("mahd"));
-                    ct.setMave(rs.getInt("mave"));
-                    ct.setMacb(rs.getInt("macb"));
-                    ct.setMaghe(rs.getInt("maghe"));
-                    ct.setGiave(rs.getDouble("giave"));
-
-                    insertTicket(ct);
-                }
-            } else {
-                System.out.println("Lỗi thêm hóa đơn!");
-            }
-        } catch (Exception e) {
-            System.out.println("Lỗi thêm hóa đơn và vé!");
-            e.printStackTrace();
-        }
-        return mahd;
-    }
-
-    public static int insertTicket(Ticket c) {
-        int row = 0;
-        String sql = "insert into ve values(?,?,?,?,?)";
-        try {
-            PreparedStatement psi = con.prepareStatement(sql);
-            psi.setInt(1, c.getMave());
-            psi.setInt(2, c.getMahd());
-            psi.setInt(3, c.getMacb());
-            psi.setInt(4, c.getMaghe());
-            psi.setDouble(5, c.getGiave());
-            row = psi.executeUpdate();
-            psi.close();
-            /*
-            if (row != 0) {
-                System.out.println("Them Ticket thanh cong !");
-            }
-             */
-        } catch (Exception e) {
-            System.out.println("Lỗi thêm vé!");
-            e.printStackTrace();
-        }
-        return row;
-    }
-
-    public static void xuatHD(int mahd) {
-        try {
-            // Đường dẫn đến nơi muốn xuất file + tên file.txt
-            FileWriter fw = new FileWriter("C:\\Users\\LENOVO\\Desktop\\TestXuatHD.txt");
-
-            Receipt h = getHD(mahd);
-            fw.write("Welcome to My Website! \r\n");
-            SimpleDateFormat fm = new SimpleDateFormat("dd-MM-yyyy");
-            fw.write("Ngày: " + fm.format(h.getNgayhd()) + "\r\n");
-            fw.write("Khách Hàng: " + CustomerDao.getTenKH(h.getMakh()) + "\r\n");
-            fw.write("Mã Vé \tMã CB \tMã Ghế \tGiá Vé \r\n");
-            fw.write("Tên Sách \t\tSố Lượng \tThành Tiền \r\n");
-            for (int i = 0; i < h.getVe().size(); i++) {
-                fw.write(h.getVe().get(i).getMave() + "\t");
-                fw.write(h.getVe().get(i).getMacb() + "\t");
-                fw.write(h.getVe().get(i).getMaghe() + "\t");
-                fw.write(h.getVe().get(i).getGiave() + "VNĐ \t");
-            }
-            fw.write("\r\nTổng cộng: \t" + h.getDonGia() + "VNĐ \t");
-            fw.close();
-        } catch (Exception e) {
-            System.out.println("Lỗi xuất hd" + e);
-        }
-        System.out.println("Success...");
-    }
 }
